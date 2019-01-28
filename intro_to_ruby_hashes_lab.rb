3fe4ada4ee129a53346b1a23b41da2285f3060b7
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-28 05:49:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,9 @@ actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {:railroads => {:pieces => "4", :names => {}, :rent_in_dollars => { }
+	  }
+	}
 
 end
 
